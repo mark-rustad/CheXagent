11796813afd7ee6c876d6cf33dc06d905f6ec039
Mark Rustad <mark.rustad@nih.gov>
2024-03-20 15:21:03 -0400

    add python script using chexagent with DataSet and DataLoader

--- cheXagent_classifier.py
+++ cheXagent_classifier.py
@@ -2,8 +2,10 @@ import pandas as pd
 import torch
 from PIL import Image
 from torch.utils.data import DataLoader, Dataset
-from transformers import AutoModelForCausalLM, AutoProcessor, GenerationConfig
+from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
+from transformers import AutoModelForCausalLM, AutoProcessor, GenerationConfig
+from tqdm import tqdm
 
 device = "cuda"
 dtype = torch.float16
@@ -15,34 +17,28 @@ model = AutoModelForCausalLM.from_pretrained("StanfordAIMI/CheXagent-8b", torch_
 )
 generation_config = GenerationConfig.from_pretrained("StanfordAIMI/CheXagent-8b")
 
+# TODO: fix how inputs are fed to generate() when using batches and DataLoader
 
 class ChestXrayDataset(Dataset):
-    def __init__(self, csv_file, img_dir, transform=None):
-        self.data_frame = pd.read_csv(
-            csv_file,
-            usecols=["image_path", "finding_labels"],
-            nrows=1e4,
-        )
-        self.img_dir = img_dir
-        self.transform = transform or transforms.Compose(
-            [
-                transforms.ToTensor(),
-            ]
-        )
+    def __init__(self, csv_file, transform=None):
+        self.data_frame = pd.read_csv(csv_file, nrows=1e4)
+        self.transform = transform or transforms.Compose([transforms.ToTensor()])
 
     def __len__(self):
         return len(self.data_frame)
 
     def __getitem__(self, idx):
-        img_name = f"{self.data_frame.iloc[idx, 1]}"
-        image = Image.open(img_name).convert("RGB")
+        img_path = f"{self.data_frame.iloc[idx, -1]}"
+        img_name = f"{self.data_frame.iloc[idx, 0]}"
+        label = f"{self.data_frame.iloc[idx, 1]}"
+        image = Image.open(img_path).convert("RGB")
         if self.transform:
             image = self.transform(image)
-        return image
+        return image, img_name, label
 
 
 def generate(images, prompt, processor, model, device, dtype, generation_config):
-    inputs = processor(images=images[:2], text=f" USER: <s>{prompt} ASSISTANT: <s>", return_tensors="pt").to(
+    inputs = processor(images=images, text=f" USER: <s>{prompt} ASSISTANT: <s>", return_tensors="pt").to(
         device=device, dtype=dtype
     )
     output = model.generate(
@@ -56,13 +52,9 @@ def generate(images, prompt, processor, model, device, dtype, generation_config)
 
 
 # Initialize the dataset and DataLoader
-dataset = ChestXrayDataset(
-    csv_file="./output/disease_classification_QA_prompts.csv",
-    img_dir="./data/NIH_Chest_X-ray_Dataset/images_001/images",
-)
-data_loader = DataLoader(dataset, batch_size=4, shuffle=True)
+dataset = ChestXrayDataset(csv_file="./output/single_finding_datalist.csv")
+data_loader = DataLoader(dataset, batch_size=1)
 
-# Fixed prompt for all images
 prompt = """A single finding is present in the given chest X-ray. Identify the finding by selecting one option from the following list:
     A) Atelectasis
     B) Cardiomegaly
@@ -81,12 +73,17 @@ prompt = """A single finding is present in the given chest X-ray. Identify the f
     O) No Finding
 Do not select multiple findings."""
 
-# Iterate over the DataLoader
-for images in data_loader:
-    # Generate responses for each image using the fixed prompt
+writer = SummaryWriter("runs/responses_log")
+total_responses = 0
+for batch in tqdm(data_loader, desc="Processing images"):
+    images, img_names, labels = batch
     responses = [
         generate(image.unsqueeze(0), prompt, processor, model, device, dtype, generation_config) for image in images
     ]
-    # Do something with the responses, like printing or logging them
-    for response in responses:
-        print(response)
+
+    for img_name, label, response in zip(img_names, labels, responses):
+        print(f"Image: {img_name}, Label: {label}, Response: {response}")
+        writer.add_text("Info", f"Image: {img_name}, Label: {label}, Response: {response}", total_responses)
+        total_responses += 1
+
+writer.close()
